2d92f429ad98fc0a8e0cf3fc8155809edcecf353
vagrant <vagrant@localhost.localdomain>
2022-09-08 22:41:25 +0000

    update bash

--- bash-step31.sh
+++ bash-step31.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir /tmp/dockerfile
 git clone git@github.com:vnghng/dockerfile.git /tmp/dockerfile
-sed -i 's/FFintech Company/Ha Noi/' /tmp/dockerfile/text.txt
+sed -i 's/FFintech Company/Nghia/' /tmp/dockerfile/text.txt
 sudo docker build -t test:latest /tmp/dockerfile
 ID=$(sudo docker run -dt test:latest)
-sudo docker logs $ID >> /tmp/logs2
+sudo docker logs $ID >> /tmp/logs1
 rm -rf /tmp/dockerfile
 echo "Done!!"  
 
