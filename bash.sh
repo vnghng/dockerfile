9cf37557915f8879f85139992614294f9cbd7a8a
vagrant <vagrant@localhost.localdomain>
2022-09-08 15:37:26 +0000

    Update file bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git clone git@github.com:vnghng/dockerfile.git /tmp
+mkdir /tmp/dockerfile
+git clone git@github.com:vnghng/dockerfile.git /tmp/dockerfile
 sed -i 's/FFintech Company/Viet Nam/' /tmp/dockerfile/text.txt
 sudo docker build -t test:latest /tmp/dockerfile
 ID=$(sudo docker run -dt test:latest)
-sudo docker logs $ID > logs  
+sudo docker logs $ID >> /tmp/logs
+rm -rf /tmp/dockerfile
+echo "Done!!"  
 
