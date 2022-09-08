717d92057e099664ab919f0ad39ba36e1ecb2f48
vagrant <vagrant@localhost.localdomain>
2022-09-08 15:15:20 +0000

    upload bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone git@github.com:vnghng/dockerfile.git /tmp
+sed -i 's/FFintech Company/Viet Nam/' tmp/dockerfile/text.txt
+sudo docker build -t test:latest tmp/dockerfile
+ID=$(sudo docker run -dt test:latest)
+sudo docker logs $ID > logs  
+
